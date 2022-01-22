501ea13b8ea46a51964ace9bd8a1b3220f65f5f6
Ryan Arora <arora.ryan@gmail.com>
2022-01-22 16:45:39 -0500

    chore: initial commit

---
+++ bin/generateEnvTypes.js
@@ -0,0 +1,21 @@
+const yargs = require("yargs");
+const generateFiles = require("../lib");
+
+const argv = yargs
+  .options({
+    envPath: {
+      default: ".env",
+      description: "Path of the .env file",
+    },
+    definitionPath: {
+      default: "typings/env.d.ts",
+      description: "Path to write the env.d.ts file",
+    },
+    examplePath: {
+      default: ".example.env",
+      description: "Path to write the .example.env file",
+    },
+  })
+  .parseSync();
+
+generateFiles(argv.envPath, argv.definitionPath, argv.examplePath);
